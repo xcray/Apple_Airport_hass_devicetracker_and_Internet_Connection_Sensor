a0a1b708d90e2e74d6d1d6b19500626576b44404
xcray <xguang.zh@icloud.com>
2018-07-01 21:56:50 +0800

    Create apple_airport.py
    If no other device_trackers configured, and want to modify values of "new_device_defaults", then we can use this dump tracker in the configuration.yaml

---
+++ custom_components/device_tracker/apple_airport.py
@@ -0,0 +1,10 @@
+"""
+Dumb platform for the Device tracker component, modified from demo.py, for apple_airport.
+"""
+
+from homeassistant.components.device_tracker import DOMAIN
+
+
+def setup_scanner(hass, config, see, discovery_info=None):
+
+    return True
